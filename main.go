84d0cff569dd7e9701de641bd4fe091b066dafc4
Daisuke Kuwabara <daisuke0582@gmail.com>
2025-10-03 12:49:36 +0200

    Add hello command for v0.2.0

--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 )
 
 var version = "dev"
 
 func main() {
-    if len(os.Args) > 1 && os.Args[1] == "version" {
-        fmt.Printf("test-cli version %s\n", version)
-        return
-    }
-    
-    fmt.Println("Hello from test-cli! 🚀")
-    fmt.Println("Run 'test-cli version' to see the version")
-}	
+	if len(os.Args) > 1 && os.Args[1] == "version" {
+		fmt.Printf("test-cli version %s\n", version)
+		return
+	}
+
+	if len(os.Args) > 1 && os.Args[1] == "hello" {
+		fmt.Println("Hello, World! 🌍")
+		fmt.Println("This is a NEW feature in v0.2.0!")
+		return
+	}
+
+	fmt.Println("Hello from test-cli! 🚀")
+	fmt.Println("Commands:")
+	fmt.Println("  test-cli version  - Show version")
+	fmt.Println("  test-cli hello    - New greeting command")
+}
